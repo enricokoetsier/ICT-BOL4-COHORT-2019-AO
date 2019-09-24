55f9d69e6b048f40bef099bf1664346c66f9a8b9
Enrico Koetsier <99058205@mydavinci.nl>
2019-09-24 12:23:05 +0200

    Begonnen met game

---
+++ Labs/Introductie4.js
@@ -0,0 +1,22 @@
+var food = "Apple";
+
+if (food == "Apple" ) {
+
+alert ("We ate some fruit and specifically an Apple")
+}
+
+else if (food == "Meat") {
+
+alert ("We eat some meat")
+}
+
+else if (food == "Fast food") {
+
+    alert ("We eat some Fast food")
+    }
+
+else {
+
+    alert ("We did not eat fruit")
+
+}
---
+++ Q1 Week 4/mijnGame.js
@@ -0,0 +1,8 @@
+alert ("Welkom in Weird Adventures")
+
+//De speler kan hier zn naam invullen
+var name = prompt('Wat is je spelersnaam?');
+alert ("Hallo "+ name +", Welkom in Weird Adventures. In de game krijg je verschillende vragen te zien die je moet beantwoorden om naar het einde te komen.")
+
+//Verhaal begint hier en krijg meteen eerste vraag
+var vraag = prompt("Je word opeens wakker op een onbewoond eiland")
